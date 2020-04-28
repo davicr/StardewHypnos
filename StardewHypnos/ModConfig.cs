6a2b1145295259feb6147e4e67ad7058e6554c07
Davi <davicr38@gmail.com>
2020-04-28 12:14:59 -0300

    Added XML documentation, changed configuration style and added a blacklist for specific bed tiles

--- StardewHypnos/ModConfig.cs
+++ StardewHypnos/ModConfig.cs
@@ -1,29 +1,205 @@
 ﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
 namespace StardewHypnos
 {
-    enum MinimumRelationshipType
+    /// <summary>
+    /// Represents the minimum relationship types.
+    /// </summary>
+    internal enum MinimumRelationshipType
     {
+        /// <summary>
+        /// NPC must be dating, engaged or married with the Farmer
+        /// </summary>
         OnlyPartners,
+
+        /// <summary>
+        /// NPC must be friends with the Farmer
+        /// </summary>
         Friends,
-        Everyone
+
+        /// <summary>
+        /// NPC must have no bond with the Farmer
+        /// </summary>
+        Everyone,
     }
 
-    class ModConfig
+    /// <summary>
+    /// The mod configuration.
+    /// </summary>
+    internal class ModConfig
     {
-        // Minimum "relationship" tier to benefit from entering friend / dating / ... NPC homes at any time
+        /// <summary>
+        /// Gets or sets the "minimum" relationship type to use new features.
+        /// </summary>
         public MinimumRelationshipType MinimumRelationship { get; set; } = MinimumRelationshipType.OnlyPartners;
 
-        // Minimum hearts with a NPC to benefit from entering their house at all times (for Friends)
+        /// <summary>
+        /// Gets or sets the minimum amount of hearts with a NPC to enter their houses and sleep there.
+        /// </summary>
         public int MinimumFriendshipHearts { get; set; } = 6;
 
-        // Open doors at all times for locations owned by NPCs with enough relation?
+        /// <summary>
+        /// Gets or sets a value indicating whether doors should be open at all times for locations owned by allowed NPCs.
+        /// </summary>
         public bool KeepFriendDoorsOpen { get; set; } = true;
 
-        // Additional warps and their NPC owners
-        public Dictionary<string, List<string>> CustomOwnerByWarp { get; set; }
+        /// <summary>
+        /// Gets or sets a collection of possible allowed NPCs in certain location.
+        /// </summary>
+        public Dictionary<string, List<string>> NPCsByWarp { get; set; } = new Dictionary<string, List<string>>
+        {
+            {
+                "AnimalShop",
+                new List<string>
+                {
+                    "Marnie",
+                    "Shane",
+                }
+            },
+            {
+                "SeedShop",
+                new List<string>
+                {
+                    "Abigail",
+                    "Pierre",
+                    "Caroline",
+                }
+            },
+            {
+                "Trailer",
+                new List<string>
+                {
+                    "Penny",
+                    "Pam",
+                }
+            },
+            {
+                "HaleyHouse",
+                new List<string>
+                {
+                    "Haley",
+                    "Emily",
+                }
+            },
+            {
+                "LeahHouse",
+                new List<string>
+                {
+                    "Leah",
+                }
+            },
+            {
+                "ScienceHouse",
+                new List<string>
+                {
+                    "Maru",
+                    "Robin",
+                    "Demetrius",
+                    "Sebastian",
+                }
+            },
+            {
+                "JoshHouse",
+                new List<string>
+                {
+                    "Alex",
+                }
+            },
+            {
+                "ElliottHouse",
+                new List<string>
+                {
+                    "Elliott",
+                }
+            },
+            {
+                "SamHouse",
+                new List<string>
+                {
+                    "Sam",
+                    "Kent",
+                    "Jodi",
+                }
+            },
+            {
+                "ManorHouse",
+                new List<string>
+                {
+                    "Lewis",
+                }
+            },
+            {
+                "SebastianRoom",
+                new List<string>
+                {
+                    "Sebastian",
+                }
+            },
+            {
+                "HarveyRoom",
+                new List<string>
+                {
+                    "Harvey",
+                }
+            },
+            {
+                "Saloon",
+                new List<string>
+                {
+                    "Gus",
+                }
+            },
+            {
+                "Tent",
+                new List<string>
+                {
+                    "Linus",
+                }
+            },
+            {
+                "Blacksmith",
+                new List<string>
+                {
+                    "Clint",
+                }
+            },
+        };
 
-        // Blacklisted warps
-        public List<string> BlacklistedWarps { get; set; }
+        /// <summary>
+        /// Gets or sets a collection of bed TileIndexes by map names.
+        /// </summary>
+        public Dictionary<string, List<int>> BedTileIndexes { get; set; } = new Dictionary<string, List<int>>
+        {
+            {
+                "townInterior",
+                new List<int> { 384, 386, 390, 448, 450, 454, 836, 1123, 1294 }
+            },
+            {
+                "ElliottHouseTiles",
+                new List<int> { 25 }
+            },
+        };
+
+        /// <summary>
+        /// Gets or sets a collection of blacklisted bed locations by map names.
+        /// </summary>
+        public Dictionary<string, List<Vector2>> BlacklistedBedTiles { get; set; } = new Dictionary<string, List<Vector2>>
+        {
+            {
+                "AnimalShop",
+                new List<Vector2>
+                {
+                    new Vector2(1, 7),
+                }
+            },
+            {
+                "SamHouse",
+                new List<Vector2>
+                {
+                    new Vector2(8, 22),
+                }
+            },
+        };
     }
 }
